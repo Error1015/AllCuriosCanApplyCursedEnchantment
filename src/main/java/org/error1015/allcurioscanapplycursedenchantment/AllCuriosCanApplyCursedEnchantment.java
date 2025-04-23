dd3e42f09102df300e43b95c876931801abc0795
Error1015 <156535541+Error1015@users.noreply.github.com>
2025-04-23 22:24:55 +0800

    ...

--- src/main/java/org/error1015/allcurioscanapplycursedenchantment/AllCuriosCanApplyCursedEnchantment.java
+++ src/main/java/org/error1015/allcurioscanapplycursedenchantment/AllCuriosCanApplyCursedEnchantment.java
@@ -2,11 +2,11 @@ package org.error1015.allcurioscanapplycursedenchantment;
 
 import net.minecraftforge.fml.common.Mod;
 
-@Mod(Allcurioscanapplycursedenchantment.MODID)
-public class Allcurioscanapplycursedenchantment {
+@Mod(AllCuriosCanApplyCursedEnchantment.MODID)
+public class AllCuriosCanApplyCursedEnchantment {
     public static final String MODID = "allcurioscanapplycursedenchantment";
 
-    public Allcurioscanapplycursedenchantment() {
+    public AllCuriosCanApplyCursedEnchantment() {
 
     }
 }
