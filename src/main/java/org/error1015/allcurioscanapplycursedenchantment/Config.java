e6d0ec38ce5e61b5d4c6210bf048fe22ad73600a
Error1015 <156535541+Error1015@users.noreply.github.com>
2025-05-19 22:10:50 +0800

    a

--- src/main/java/org/error1015/allcurioscanapplycursedenchantment/Config.java
+++ src/main/java/org/error1015/allcurioscanapplycursedenchantment/Config.java
@@ -2,13 +2,10 @@ package org.error1015.allcurioscanapplycursedenchantment;
 
 import net.minecraftforge.common.ForgeConfigSpec;
 import net.minecraftforge.fml.common.Mod;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 @Mod.EventBusSubscriber(modid = AllCuriosCanApplyCursedEnchantment.MODID, bus = Mod.EventBusSubscriber.Bus.MOD)
 public class Config {
     public static final ForgeConfigSpec.Builder builder = new ForgeConfigSpec.Builder();
-    private static final Logger log = LoggerFactory.getLogger(Config.class);
     public static ForgeConfigSpec spec;
 
     public static ForgeConfigSpec.BooleanValue enable = builder.define("enable", true);
@@ -16,9 +13,4 @@ public class Config {
     static {
         spec = builder.build();
     }
-
-    // @SubscribeEvent
-    // public static void onLoad(final ModConfigEvent.Loading configEvent) {
-    //     enable = builder.define("enable", true);
-    // }
 }
