fd8dcf50ea0cde9b2148955f5be1323c4a39dc85
Error1015 <156535541+Error1015@users.noreply.github.com>
2025-05-19 14:05:02 +0800

    添加config

--- src/main/java/org/error1015/allcurioscanapplycursedenchantment/AllCuriosCanApplyCursedEnchantment.java
+++ src/main/java/org/error1015/allcurioscanapplycursedenchantment/AllCuriosCanApplyCursedEnchantment.java
@@ -1,10 +1,15 @@
 package org.error1015.allcurioscanapplycursedenchantment;
 
+import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.config.ModConfig;
 
 @Mod(AllCuriosCanApplyCursedEnchantment.MODID)
 public class AllCuriosCanApplyCursedEnchantment {
     public static final String MODID = "allcurioscanapplycursedenchantment";
 
-    public AllCuriosCanApplyCursedEnchantment() {}
+    public AllCuriosCanApplyCursedEnchantment() {
+        ModLoadingContext modLoadingContext = ModLoadingContext.get();
+        modLoadingContext.registerConfig(ModConfig.Type.COMMON, Config.builder.build());
+    }
 }
--- src/main/java/org/error1015/allcurioscanapplycursedenchantment/Config.java
+++ src/main/java/org/error1015/allcurioscanapplycursedenchantment/Config.java
@@ -8,11 +8,13 @@ import net.minecraftforge.fml.event.config.ModConfigEvent;
 @Mod.EventBusSubscriber(modid = AllCuriosCanApplyCursedEnchantment.MODID, bus = Mod.EventBusSubscriber.Bus.MOD)
 public class Config {
     public static final ForgeConfigSpec.Builder builder = new ForgeConfigSpec.Builder();
+    public static ForgeConfigSpec spec;
 
     public static boolean enable;
 
     @SubscribeEvent
     public static void onLoad(final ModConfigEvent.Loading configEvent) {
         enable = builder.define("enable", true).get();
+        spec = builder.build();
     }
 }
